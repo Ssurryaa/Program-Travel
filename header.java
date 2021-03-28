69a3e0a1851ce8a3249633b4407d4160a0bec175
Ssurryaa <75293376+Ssurryaa@users.noreply.github.com>
2021-03-28 21:20:36 +0700

    Add files via upload

---
+++ header.java
@@ -0,0 +1,14 @@
+package UTS;
+
+public class header {
+    private String nama;
+
+    public String getName() {
+        return nama;
+    }
+
+    public void setName(String newNama) {
+        this.nama = newNama;
+    }
+
+}
